47502d3eb4fb1a2fefaeb9eb7180963be0913b7a
Shortytheman <shor7ty@gmail.com>
2022-10-06 12:22:45 +0200

    CustomerTests + Customerservice oppe og køre

--- src/main/java/dat3/adventure/dto/CustomerRequest.java
+++ src/main/java/dat3/adventure/dto/CustomerRequest.java
@@ -1,6 +1,7 @@
 package dat3.adventure.dto;
 
 
+import dat3.adventure.entity.Activity;
 import dat3.adventure.entity.Customer;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -26,6 +27,14 @@ public class CustomerRequest {
   public static Customer getCustomerEntity(CustomerRequest crq){
     return new Customer(crq.getCustomerId(), crq.getCustomerName(), crq.getCustomerEmail(), crq.getPhoneNumber(),
         crq.getCompanyName(), crq.getCvrNumber());
+  }
+
+  public CustomerRequest(Customer c) {
+    this.customerName = c.getCustomerName();
+    this.customerEmail = c.getCustomerEmail();
+    this.phoneNumber = c.getPhoneNumber();
+    this.companyName = c.getCompanyName();
+    this.cvrNumber = c.getCvrNumber();
   }
 
   public CustomerRequest(int customerId, String customerName, String customerEmail, String phoneNumber) {
--- src/main/java/dat3/adventure/service/CustomerService.java
+++ src/main/java/dat3/adventure/service/CustomerService.java
@@ -12,6 +12,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class CustomerService {
@@ -35,8 +36,42 @@ public class CustomerService {
     }
 
     public void editCustomer(CustomerRequest body, int id) {
+        Customer customer = customerRepository.findById(id).orElseThrow(
+            () -> new ResponseStatusException(HttpStatus.BAD_REQUEST, "En aktivitet eksisterer med dette ID"));
+
+        customer.setCustomerEmail(body.getCustomerEmail());
+        customer.setCustomerName(body.getCustomerName());
+        customer.setPhoneNumber(body.getPhoneNumber());
+        customer.setCompanyName(body.getCompanyName());
+        customer.setCvrNumber(body.getCvrNumber());
+
+        customerRepository.save(customer);
     }
 
     public List<CustomerResponse> getCustomers() {
+        List<Customer> customers = customerRepository.findAll();
+
+        List<CustomerResponse> response = customers.stream().map(customer -> {
+            if (customerRepository.findById(customer.getCustomerId()).
+                orElseThrow(() -> new ResponseStatusException(HttpStatus.BAD_REQUEST,
+                    "Kan ikke finde kunde med dette ID")).getCvrNumber() == null){
+                return new CustomerResponse(customer, false);
+            } else {
+                return new CustomerResponse(customer, true);
+            }
+        }).collect(Collectors.toList());
+
+        return response;
     }
+
+    public CustomerResponse getCustomerById(int id){
+        Customer found = customerRepository.findById(id).orElseThrow(()-> new ResponseStatusException(HttpStatus.NOT_FOUND,"Can't find the customer"));
+
+        if (found.getCustomerName().equalsIgnoreCase("null") || found.getCvrNumber() == null) {
+            return new CustomerResponse(found, false);
+        } else {
+            return new CustomerResponse(found, true);
+        }
+    }
+
 }
